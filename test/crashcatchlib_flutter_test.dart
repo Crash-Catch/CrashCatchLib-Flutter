4b06aaaf3b04040d30c43c2dc66bc28a8815a778
Chris Board <support@boardiesitsolutions.com>
2021-04-10 16:18:31 +0100

    updated file name to match pub.dev expectancy

--- test/crashcatchlib_flutter_test.dart
+++ test/crashcatchlib_flutter_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:crashcatchlib_flutter/CrashCatch.dart';
+import 'package:crashcatchlib_flutter/crashcatchlib_flutter.dart';
 
 void main() {
   test('adds one to input values', () {
